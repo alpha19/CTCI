e1bc2f26a7971db58317dc0efe94b1146ea5f8ee
kdedow <kdedow@gmail.com>
2019-04-07 22:21:51 -0700

    Add package for trees and graphs questions

---
+++ TreesAndGraphs/TreesAndGraphs.py
@@ -0,0 +1,7 @@
+"""
+Contains methods related to Trees and Graphs Questions from Cracking the Coding Interview
+"""
+class TreesAndGraphsQuestions:
+    """
+    This class is basically just a namespace
+    """
---
+++ TreesAndGraphs/UnitTests_TreesAndGraphs/UnitTests_TreesAndGraphs.py
@@ -0,0 +1,8 @@
+"""
+Tests various  stack classes and methods
+"""
+import unittest
+from TreesAndGraphs.TreesAndGraphs import *
+
+class TestTreesAndGraphsQuestions(unittest.TestCase):
+    """ Class for unit test methods """
