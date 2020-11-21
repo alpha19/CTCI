1e72984b857d8711df7f6f9f17a47cb4ad96dd3b
kdedow <kdedow@gmail.com>
2020-11-21 11:56:48 -0800

    Add simple BST node and Linked List node

--- TreesAndGraphs/TreesAndGraphs.py
+++ TreesAndGraphs/TreesAndGraphs.py
@@ -1,6 +1,16 @@
 """
 Contains methods related to Trees and Graphs Questions from Cracking the Coding Interview
 """
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class LinkedListNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
 class GraphNode:
     def __init__(self, nodeName, adj: list=None):
@@ -68,3 +78,54 @@ class TreesAndGraphsQuestions:
                 return True
 
         return False
+
+    @staticmethod
+    def ArrayToBST(arry: list):
+        """
+        Given an increasing order array with unique interger elements, write
+        an algorithm to create a BST with minimal height
+
+        :param arry: The array (increasing order) with unique integer elements
+        :return: The BST of minimal possible size
+        """
+        if len(arry) == 0:
+            return None
+
+        mid = len(arry) / 2
+        node = BSTNode(arry[mid])
+        if (mid - 1) >= 0:
+            node.left = ArrayToBST(arry[0:mid])
+        if (mid + 1) < len(arry):
+            node.right = Array[(mid+1):]
+
+        return node
+
+    @staticmethod
+    def BSTToDepthList(node: BSTNode, depth: int, arry: list):
+        """
+        Given a binary tree, design an algorithm which creates a linked list of all
+        the nodes at each depth.
+
+        :param node: The current node in the BST (starts with the root node)
+        :param depth: The depth the current node is associated with (starts at zero)
+        :param arry: The array that contains all the linked lists. Element at index zero is associated with linked list
+                     of depth 0 nodes, index one with depth 1 nodes and so on.
+        """
+        if node is None:
+            return
+
+        LinkedListNode llNode = LinkedListNode(node.value)
+
+        if len(arry) == depth:
+            arry.append(llNode)
+        else:
+            tempNode = arry[depth]
+            while tempNode.next is not None:
+                tempNode = tempNode.next
+
+            tempNode.next = llNode
+
+        BSTToDepthList(node.left, (depth + 1))
+        BSTToDepthList(node.right, (depth + 1))
+
+
