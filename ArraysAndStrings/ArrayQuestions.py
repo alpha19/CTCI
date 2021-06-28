32c5db9def56ca9ded05a607bc5fdd8336df0191
kdedow <kdedow@gmail.com>
2021-06-28 15:26:09 -0700

    Fix?

--- ArraysAndStrings/ArrayQuestions.py
+++ ArraysAndStrings/ArrayQuestions.py
@@ -4,7 +4,7 @@ Class for array based questions. This class operates more like a namespace
 """
 class ArrayQuestions:
     def __init__(self):
-        # Do nothing, each method is to be static for now
+        pass
 
     @staticmethod
     def QuestionOne(self):
